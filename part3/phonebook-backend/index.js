5a39cd964b3dee1258c91761dda3f4a3a8ff8410
pdjorgeluis <55862290+pdjorgeluis@users.noreply.github.com>
2024-06-24 09:45:33 -0400

    phonebook-backend excercises 3.7-3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-
-app.use(express.json())
+const morgan = require('morgan')
 
 let persons = [
     { 
@@ -25,6 +24,14 @@ let persons = [
         "number": "39-23-6423122"
     }    
 ]
+
+app.use(express.json())
+
+morgan.token('body', (request, response) => {
+    const body = JSON.stringify(request.body)
+    return body !== '{}' ? body : '-';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
