497ba43d112f3401884bcbe30eac8f2d23e45c01
pdjorgeluis <55862290+pdjorgeluis@users.noreply.github.com>
2024-07-08 21:47:49 -0400

    Part 5 excercise 5.14

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { beforeEach } from 'vitest'
 
-
-test('renders title and author', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'Blog Title',
     url: 'http://thisBlog.html',
@@ -17,17 +16,29 @@ test('renders title and author', () => {
     _id: 987654
   }
 
-  const { container } = render(<Blog blog={blog} />)
+  test('renders title and author but hides url and likes', () => {
+    const { container } = render(<Blog blog={blog} />)
 
-  const div1 = container.querySelector('.blog')
-  const div2 = container.querySelector('.blogDetails')
-  expect(div1).toHaveTextContent(
-    'Blog Title'
-  )
-  expect(div1).toHaveTextContent(
-    'jito'
-  )
-  expect(div2).toHaveStyle(
-    'display: none'
-  )
+    const div1 = container.querySelector('.blog')
+    const div2 = container.querySelector('.blogDetails')
+    expect(div1).toHaveTextContent(
+      'Blog Title'
+    )
+    expect(div1).toHaveTextContent(
+      'jito'
+    )
+    expect(div2).toHaveStyle(
+      'display: none'
+    )
+  })
+
+  test('after clicking the button url and likes are displayed', async () => {
+    const { container } = render(<Blog blog={blog} />)
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.blogDetails')
+    expect(div).not.toHaveStyle('display: none')
+  })
 })
