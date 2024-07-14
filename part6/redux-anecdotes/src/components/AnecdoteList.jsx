41c5c41f2458e00dcf8c8d5fde1acfa6840646a8
pdjorgeluis <55862290+pdjorgeluis@users.noreply.github.com>
2024-07-14 13:22:55 -0400

    Part 6 exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,16 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
+import Filter from './Filter'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    return state.filter !== ''
+      ? state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+      : state.anecdotes
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
     dispatch(voteFor(id))
   }
 
+  const style = {
+    marginLeft: 10
+  }
+
   return (
     <div>
+      <Filter />
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
@@ -18,7 +28,7 @@ const AnecdoteList = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button style={style} onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
       )}
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,10 +1,22 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { filterChange } from './reducers/filterReducer'
+
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer
+})
 
 const store = createStore(reducer)
+//console.log(store.getState())
+//store.dispatch(filterChange('THE FILTER'))
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
