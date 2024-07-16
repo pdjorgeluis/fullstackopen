557b12a899cd9cee62e0e60ece93892dab0e53d9
pdjorgeluis <55862290+pdjorgeluis@users.noreply.github.com>
2024-07-15 21:16:21 -0400

    Part 6 up to exercise 6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,26 +1,61 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
-
-  const anecdotes = [
+  /*const anecdotes = [
     {
       "content": "If it hurts, do it more often",
       "id": "47145",
       "votes": 0
     },
-  ]
+  ]*/
+
+  const queryClient = useQueryClient()
+
+ const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote, 
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
+
+   const handleVote = (anecdote) => {
+    console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1 })
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+  
+  if (result.isError) {
+    return (
+      <span>
+        anecdote service is not available due to problems in server<br/>
+        Error: {result.error.message} 
+      </span>
+    )
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
     
       <Notification />
-      <AnecdoteForm />
+      <AnecdoteForm anecdotes={anecdotes} />
     
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,24 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient  } from "@tanstack/react-query"
+import { getAnecdotes, createAnecdote, updateAnecdote } from '../requests'
 
-  const onCreate = (event) => {
+const AnecdoteForm = ({ anecdotes }) => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      //const anecdotes = queryClient.getQueryData({ queryKey: ['anecdotes'] })
+      //queryClient.setQueryData({ queryKey: ['anecdotes'] }, anecdotes.concat(newAnecdote))
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+
+  const onCreate = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdoteMutation.mutate({content, votes:0})
 }
 
   return (
